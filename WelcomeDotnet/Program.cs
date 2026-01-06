d7435cd68e69e2391eab66abe7fc36ae3e421a9a
kira23j <kirubelmamuyesahle@gmail.com>
2026-01-06 15:12:50 +0300

    feat: adding WelcomeDotnet project with a basic console application

---
+++ WelcomeDotnet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WelcomeDotnet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=================================");
+            Console.WriteLine(" Welcome to C# .NET Simple Project");
+            Console.WriteLine("=================================");
+            Console.WriteLine();
+            Console.WriteLine("This project demonstrates a basic .NET console application.");
+        }
+    }
+}
